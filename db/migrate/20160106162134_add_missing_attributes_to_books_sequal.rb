f9c5a82bb5263891a247988f0e7cc6a4290e14e3
jonassvalin <jonassvalin@gmail.com>
2016-01-06 18:05:57 +0100

    skipped db migration

--- db/migrate/20160106162134_add_missing_attributes_to_books_sequal.rb
+++
@@ -1,8 +0,0 @@
-class AddMissingAttributesToBooksSequal < ActiveRecord::Migration
-  def change
-    add_column :books, :author, :string
-    add_column :books, :review, :text
-    add_column :books, :genre_id, :integer
-    add_column :books, :amazon_id, :string
-  end
-end
