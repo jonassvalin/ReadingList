060e050e706fa35f6187ededf3d4f00c6195f656
jonassvalin <jonassvalin@gmail.com>
2016-01-06 17:56:47 +0100

    Created API usage description page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :books
-  resources :finished_books
-  resources :genres
+  resources :books, only: [:index, :create, :destroy]
+  resources :finished_books, only: :index
+  #resources :genres, only:
 end
