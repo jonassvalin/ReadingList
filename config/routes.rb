f1724940bc808d97e64d6ac6d254b3039184585b
jonassvalin <jonassvalin@gmail.com>
2016-01-06 16:41:16 +0100

    create new book

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :books, only: :index
+  resources :books, except: :destroy
   resources :finished_books
   resources :genres
 end
--- test/integration/creating_books_test.rb
+++ test/integration/creating_books_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class CreatingBooksTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'create new book with valid data' do
+    title = 'Pragmatic Programmer'
+    rating = 5
+
+    post '/books', { book: {
+      title: title,
+      rating: rating
+    } }.to_json, { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+
+    assert_equal 201, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    book = json(response.body)
+    assert_equal book_url(book[:id]), response.location
+
+    assert_equal title, book[:title]
+    assert_equal rating, book[:rating].to_i
+  end
+
+  test 'does not create valid data' do
+    title = nil
+    rating = 5
+
+    post '/books', { book: {
+      title: title,
+      rating: rating
+    } }.to_json, { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
+
+    assert_equal 422, response.status
+    assert_equal Mime::JSON, response.content_type
+  end
 end
